9733ad5d46a75d59d64b43420265c15da39ff259
NIHARIKA-NK <123749184+NIHARIKA-NK@users.noreply.github.com>
2025-04-30 11:54:10 +0530

    Add files via upload

---
+++ PES1UG22CS383_Jenkins/main/hello.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "This is PES1UG22CS383\n";
+  cout << "After webhooks";
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
